468a294cb072495862188131acf0ac1137243450
yunjin544 <yunjin544@gmail.com>
2022-08-30 23:54:16 +0900

    first commit

--- vesc_test_gui_ecat/build/CMakeFiles/clean_additional.cmake
+++
@@ -1,7 +0,0 @@
-# Additional clean files
-
-file(REMOVE_RECURSE
-  "CMakeFiles/vesc_test_gui_ecat_autogen.dir/AutogenUsed.txt"
-  "CMakeFiles/vesc_test_gui_ecat_autogen.dir/ParseCache.txt"
-  "vesc_test_gui_ecat_autogen"
-)
